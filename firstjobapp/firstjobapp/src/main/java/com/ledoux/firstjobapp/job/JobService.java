885cc3d8a89d1aa6a55c549c928e99d043571c79
Gustavo Simas Ledoux <gsledoux@gmail.com>
2024-02-20 01:37:11 -0300

    [JobApp] - Create JobService and it's  Impl
    Added JobService and JobServiceImpl;
    Added Post and Get methods on Controlles;

---
+++ firstjobapp/firstjobapp/src/main/java/com/ledoux/firstjobapp/job/JobService.java
@@ -0,0 +1,11 @@
+package com.ledoux.firstjobapp.job;
+
+import java.util.List;
+
+public interface JobService {
+
+    List<Job> findAll();
+    void createJob(Job job);
+
+    Job getJobById(Long id);
+}
